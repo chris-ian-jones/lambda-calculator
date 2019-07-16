1acc1d668f638a345a4284117d39a4f97d6922c9
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-16 21:14:15 +0100

    WIP: still need to figure mapping numbers to buttons

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -13,21 +13,16 @@ import { numbers } from '../../../data'
 const Numbers = () => {
   // STEP 2 - add the imported data to state
   const [numberState, setNumberState] = useState(numbers)
-  // console.log(numbers)
+  
   return (
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/
-       
-      numbers.map(number => <NumberButton number={number}/>) 
-       
-       }
-
-       
-       
-       
-
+      it any props needed by the child component*/}
+      //  console.log(numbers)
+      {numbers.map(number => (<NumberButton number={number}/>))}
     </div>
   );
 };
+
+export default Numbers
