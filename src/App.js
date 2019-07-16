e9cc3180e94ddfbe2155fe61c038955273bd69de
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-16 21:34:44 +0100

    WIP: still need to figure mapping numbers to buttons

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import "./App.css";
 import "./data"
 import "./components/ButtonComponents/NumberButtons/Numbers"
+import "./components/ButtonComponents/NumberButtons/NumberButton"
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
 // Logo has already been provided for you. Do the same for the remaining components
 import Logo from "./components/DisplayComponents/Logo";
+import { numbers } from "./data";
 
 function App() {
   // STEP 5 - After you get the components displaying using the provided data file, write your state hooks here.
@@ -20,6 +22,7 @@ function App() {
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Numbers />
       </div>
     </div>
   );
